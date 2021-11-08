c12d89b5e053b9d031e8c862e7c2fb08ca7b25c6
Raghavendra17N <raghava01995@gamil.com>
2021-11-08 14:23:50 +0530

    sonarqube

--- Linux-VM/startup_script.sh
+++ Linux-VM/startup_script.sh
@@ -16,18 +16,10 @@ else
   sudo apt install ansible -y 
 fi
 
-sudo apt-get update && sudo apt-get install default-jdk -y
+sudo sh -c "apt update && yes | apt install openjdk-11-jdk && java -version"
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 sudo wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 sudo apt-get -y install postgresql postgresql-contrib
-
-sed -i -e '1ilocal    postgres     postgres     peer\' /etc/postgresql/10/main/pg_hba.conf
-
-echo "postgres:postgres" | chpasswd
-echo "postgres" | passwd --stdin postgres
-service postgresql restart
-sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
-sudo systemctl enable postgresql
 
 
 sudo systemctl start postgresql
@@ -39,41 +31,27 @@ sudo -u postgres psql -c  "ALTER USER sonar WITH ENCRYPTED password 'Raghu@123';
 sudo -u postgres psql -c  "CREATE DATABASE sonarqube OWNER sonar;"
 sudo -u postgres psql -c  "GRANT ALL PRIVILEGES ON DATABASE sonarqube to sonar;"
 
-sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.6.0.39681.zip
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.1.0.47736.zip
 sudo apt-get -y install unzip
 sudo unzip sonarqube*.zip -d /opt
-sudo mv /opt/sonarqube-8.6.0.39681 /opt/sonarqube -v
+sudo mv /opt/sonarqube-9.1.0.47736 /opt/sonarqube -v
+
+sudo groupadd sonar
+sudo useradd -c "Sonar System User" -d /opt/sonarqube -g sonar -s /bin/bash sonar
+sudo chown -R sonar:sonar /opt/sonarqube
+sudo chmod 775 /opt/sonarqube
 
 sudo sed -i -e 's/#sonar.jdbc.username=/sonar.jdbc.username=sonar/g' /opt/sonarqube/conf/sonar.properties
 sudo sed -i -e 's/#sonar.jdbc.password=/sonar.jdbc.password=Raghu@123/g' /opt/sonarqube/conf/sonar.properties
 echo "sonar.jdbc.url=jdbc:postgresql://localhost/sonarqube" >> /opt/sonarqube/conf/sonar.properties
 
-echo "[Unit]
-Description=SonarQube service
-After=syslog.target network.target
-
-[Service]
-Type=forking
-
-ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
-ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
-LimitNOFILE=131072
-LimitNPROC=8192
-User=sonar
-Group=sonarGroup
-Restart=always
-
-[Install]
-WantedBy=multi-user.target" | sudo tee -a /etc/systemd/system/sonar.service
 
 echo "vm.max_map_count=262144" >> /etc/sysctl.conf
-echo "fs.file-max=65536" >> /etc/sysctl.conf
+echo "fs.file-max=262144" >> /etc/sysctl.conf
 
-echo "sonar   -   nofile   65536" >> /etc/security/limits.conf
-echo "sonar   -   nproc    4096" >> /etc/security/limits.conf
+sysctl -p
 
-sudo sysctl -p
+su - sonar -c " sh /opt/sonarqube/bin/linux-x86-64/sonar.sh start"
 
-sudo service sonarqube start
-sudo systemctl enable sonar
+
 
